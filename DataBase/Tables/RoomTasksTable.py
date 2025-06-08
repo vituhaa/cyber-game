a8a230a2ad86a9db20ad58a629b0329609bfd735
dinonikoo <eskamaeva@edu.hse.ru>
2025-06-08 20:25:50 +0300

    get_last_task_in_room added

--- DataBase/Tables/RoomTasksTable.py
+++ DataBase/Tables/RoomTasksTable.py
@@ -20,3 +20,16 @@ def add_task_to_room(room_id, task_id):
     with connect() as conn:
         cur = conn.cursor()
         cur.execute("INSERT INTO Room_Tasks (room_id, task_id) VALUES (?, ?)", (room_id, task_id))
+
+def get_last_task_in_room(room_id):
+    with connect() as conn:
+        cur = conn.cursor()
+        cur.execute("""
+            SELECT task_id
+            FROM Room_Tasks
+            WHERE room_id = ?
+            ORDER BY id DESC
+            LIMIT 1
+        """, (room_id,))
+        result = cur.fetchone()
+        return result[0] if result else None
